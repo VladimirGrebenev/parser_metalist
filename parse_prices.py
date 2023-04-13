89a026a7a86c48f9e5ab5c592a588f62d8c911de
Vladimir Grebenev <grebenev-81@mail.ru>
2023-04-13 20:11:00 +0300

    TO DO: разобраться с загрузкойй данных с металлобаз - нужно грузить в отдельные категории и заголовки взять со span.

--- parse_prices.py
+++ parse_prices.py
@@ -1,3 +1,5 @@
+"""Сборщик прайсов с сайта"""
+
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
@@ -11,30 +13,40 @@ def main():
     """Главная функция запуска"""
     price_links = get_price_links(price_url, price_pattern)
     for link in price_links:
-        tables_list = parse_price_link(link)
-        parse_tables(tables_list)
+        tables_dict = parse_price_link(link)
+        parse_tables(tables_dict)
 
 
 def get_price_links(prc_url, prc_ptn):
     """
     Функция принимает ссылку на страницу с прайсами и паттерн для поиска
-    ссылок с прайсами. Возвращает список со ссылками на прайсы.
+    ссылок с прайсами. Возвращает словарь со ссылками на прайсы.
     :param prc_url: Ссылка на страницу 'https://mc.ru/price/msk#'
     :param prc_ptn: Паттерн ссылки 'https://mc.ru/prices/'
-    :return: Список ссылок.
+    :return: Словарь ссылок.
     """
 
     response = requests.get(prc_url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    soup_links = soup.find('ul', class_='pagePriceList').find_all('a')
-    price_links = []
+    # soup_links_titles = soup.find('ul', class_='pagePriceList').find_all(
+    #     'li')[1:-1]
+    soup_links = soup.find('ul', class_='pagePriceList').find_all('a')[2:-1]
+
+    prc_links = []
+    # title_links = []
+
+    # for title in soup_links_titles:
+    #     if title.text:
+    #         title_links.append(title.text.replace('XLS\nHtml\n', '').strip())
 
     for link in soup_links:
         if link.get('href') and link.get('href').startswith(prc_ptn) \
                 and link.get('href')[-4:] == '.htm':
-            price_links.append(link.get('href'))
+            prc_links.append(link.get('href'))
 
-    return price_links
+    # price_links_dict = dict(zip(title_links, prc_links))
+
+    return prc_links
 
 
 def parse_price_link(link):
@@ -42,6 +54,7 @@ def parse_price_link(link):
     Функция принимает ссылку на страницу с прайсами и отдаёт список html
     таблиц.
     :param link: Ссылка на страницу с прайсами.
+    :param main_category: Главная категория.
     :return: Список таблиц в html
     """
 
@@ -49,33 +62,53 @@ def parse_price_link(link):
     price_link = link
     response = requests.get(price_link)
     soup = BeautifulSoup(response.content, 'html.parser')
+
+    # создаём название главной категории
+    m_cat = []
+    for content in soup.select('h1'):
+        content.span.decompose()
+        m_cat.append(content.text)
+
+    if len(m_cat) != 0:
+        main_category = m_cat[0].strip()
+    else:
+        for content in soup.select('h2'):
+            m_cat.append(content.text)
+        main_category = m_cat[0].strip()
+
     tables = soup.find_all('table')
     tables_list = []
 
     for table in tables:
         tables_list.append(table)
 
-    return tables_list
+    tables_dict = {main_category: tables_list}
+
+    return tables_dict
 
 
-def parse_tables(t_list):
+def parse_tables(tables_dict):
     """
     Функция парсит таблицы и записывает каждую в отдельный файл csv
-    :param t_list: Список таблиц в HTML
+    :param t_list: словарь
+    :param m_category:главная категория
     """
+    t_list = list(tables_dict.values())[0]
+    m_category = list(tables_dict.keys())[0]
 
     # создаём заголовки таблиц
     for table in t_list:
         headers = []
+        headers.append('категория')
         for el in table.find_all('th'):
             title = el.text
             headers.append(title)
 
-        # фиксируем категорию
-        category = str(headers[0]).replace('/', '-')
+        # фиксируем подкатегории
+        sub_category = str(headers[1]).replace('/', '-')
 
-        # меняем название первой колонки
-        headers[0] = 'категория'
+        # меняем название второй колонки
+        headers[1] = 'подкатегория'
 
         # Create a dataframe
         mydata = pd.DataFrame(columns=headers)
@@ -83,17 +116,18 @@ def parse_tables(t_list):
         for d in table.find_all('tr')[2:]:
             row_data = d.find_all('td')
             table_row = []
-            table_row.append(category)
+            table_row.append(m_category)
+            table_row.append(sub_category)
             for item in row_data:
-                table_row.append(item.text)
+                table_row.append(item.text.strip())
             length = len(mydata)
             mydata.loc[length] = table_row
 
         # Export to csv
-        price_path = './price/'
+        price_path = f'./price/'
         if not os.path.exists(price_path):
             os.makedirs(price_path)
-        mydata.to_csv(fr'./price/{category}', index=False)
+        mydata.to_csv(fr'./price/{sub_category}', index=False)
 
 
 if __name__ == '__main__':
