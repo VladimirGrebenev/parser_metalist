bab633bcda70c73ba079a528e0efa3637ad5995a
Vladimir Grebenev <grebenev-81@mail.ru>
2023-04-13 22:09:59 +0300

    parser готов. нужно добавить возможность изменения цены по коэфициенту

--- parse_prices.py
+++ parse_prices.py
@@ -11,10 +11,10 @@ price_pattern = 'https://mc.ru/prices/'
 
 def main():
     """Главная функция запуска"""
-    price_links = get_price_links(price_url, price_pattern)
-    for link in price_links:
-        tables_dict = parse_price_link(link)
-        parse_tables(tables_dict)
+    price_links_dict = get_price_links(price_url, price_pattern)
+    for title, link in price_links_dict.items():
+        tables_list = parse_price_link(link)
+        parse_tables(tables_list, title)
 
 
 def get_price_links(prc_url, prc_ptn):
@@ -28,25 +28,25 @@ def get_price_links(prc_url, prc_ptn):
 
     response = requests.get(prc_url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    # soup_links_titles = soup.find('ul', class_='pagePriceList').find_all(
-    #     'li')[1:-1]
+    soup_links_titles = soup.find('ul', class_='pagePriceList').find_all(
+        'li')[1:-1]
     soup_links = soup.find('ul', class_='pagePriceList').find_all('a')[2:-1]
 
     prc_links = []
-    # title_links = []
+    title_links = []
 
-    # for title in soup_links_titles:
-    #     if title.text:
-    #         title_links.append(title.text.replace('XLS\nHtml\n', '').strip())
+    for title in soup_links_titles:
+        if title.text:
+            title_links.append(title.text.replace('XLS\nHtml\n', '').strip())
 
     for link in soup_links:
         if link.get('href') and link.get('href').startswith(prc_ptn) \
                 and link.get('href')[-4:] == '.htm':
             prc_links.append(link.get('href'))
 
-    # price_links_dict = dict(zip(title_links, prc_links))
+    price_links_dict = dict(zip(title_links, prc_links))
 
-    return prc_links
+    return price_links_dict
 
 
 def parse_price_link(link):
@@ -54,7 +54,6 @@ def parse_price_link(link):
     Функция принимает ссылку на страницу с прайсами и отдаёт список html
     таблиц.
     :param link: Ссылка на страницу с прайсами.
-    :param main_category: Главная категория.
     :return: Список таблиц в html
     """
 
@@ -64,17 +63,17 @@ def parse_price_link(link):
     soup = BeautifulSoup(response.content, 'html.parser')
 
     # создаём название главной категории
-    m_cat = []
-    for content in soup.select('h1'):
-        content.span.decompose()
-        m_cat.append(content.text)
-
-    if len(m_cat) != 0:
-        main_category = m_cat[0].strip()
-    else:
-        for content in soup.select('h2'):
-            m_cat.append(content.text)
-        main_category = m_cat[0].strip()
+    # m_cat = []
+    # for content in soup.select('h1'):
+    #     content.span.decompose()
+    #     m_cat.append(content.text)
+    #
+    # if len(m_cat) != 0:
+    #     main_category = m_cat[0].strip()
+    # else:
+    #     for content in soup.select('h2'):
+    #         m_cat.append(content.text)
+    #     main_category = m_cat[0].strip()
 
     tables = soup.find_all('table')
     tables_list = []
@@ -82,19 +81,19 @@ def parse_price_link(link):
     for table in tables:
         tables_list.append(table)
 
-    tables_dict = {main_category: tables_list}
+    # tables_dict = {main_category: tables_list}
 
-    return tables_dict
+    return tables_list
 
 
-def parse_tables(tables_dict):
+def parse_tables(t_list, m_category):
     """
     Функция парсит таблицы и записывает каждую в отдельный файл csv
     :param t_list: словарь
     :param m_category:главная категория
     """
-    t_list = list(tables_dict.values())[0]
-    m_category = list(tables_dict.keys())[0]
+    # t_list = list(tables_dict.values())[0]
+    # m_category = list(tables_dict.keys())[0]
 
     # создаём заголовки таблиц
     for table in t_list:
@@ -127,7 +126,7 @@ def parse_tables(tables_dict):
         price_path = f'./price/'
         if not os.path.exists(price_path):
             os.makedirs(price_path)
-        mydata.to_csv(fr'./price/{sub_category}', index=False)
+        mydata.to_csv(fr'./price/{m_category}-{sub_category}', index=False)
 
 
 if __name__ == '__main__':
